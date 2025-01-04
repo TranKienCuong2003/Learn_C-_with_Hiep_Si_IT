a9652c8e34477c311face17038eb7c07f621ed91
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-04 22:00:15 +0700

    Add 56.listOddNumbers.cpp file

---
+++ 56.listOddNumbers.cpp
@@ -0,0 +1,27 @@
+// Liệt kê các giá trị lẻ trong mảng các số nguyên bằng C++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Nhap so luong phan tu trong mang: ";
+    cin >> n;
+
+    int arr[n];
+    
+    cout << "Nhap " << n << " phan tu cua mang (so nguyen): ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Cac gia tri le trong mang la: ";
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0) {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
