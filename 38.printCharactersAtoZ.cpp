649ff0b267fcf68ab09a17ce70dc2f2a6852d3bb
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-04 21:53:16 +0700

    Add 38.printCharactersAtoZ.cpp file

---
+++ 38.printCharactersAtoZ.cpp
@@ -0,0 +1,12 @@
+// Sử dụng vòng lặp for để xuất các ký tự từ A đến Z bằng C++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (char c = 'A'; c <= 'Z'; c++) {
+        cout << c << " ";
+    }
+    cout << endl;
+    return 0;
+}
